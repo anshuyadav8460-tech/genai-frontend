7e40898c5e604bd5cecc958167074dcdde16fb36
Anshu Yadav <anshuyadav@8460@gmail.com>
2026-08-18 22:46:35 +0530

    Fix interview page import

--- src/app.routes.jsx
+++ src/app.routes.jsx
@@ -3,8 +3,7 @@ import Login from "./features/auth/pages/Login";
 import Register from "./features/auth/pages/Register";
 import Protected from "./features/auth/components/Protected";
 import Home from "./features/interview/pages/Home";
-import Interview from "./features/interview/pages/Interview";
-
+import Interview from "./features/interview/pages/Interview.jsx";
 
 export const router = createBrowserRouter([
     {
@@ -17,10 +16,18 @@ export const router = createBrowserRouter([
     },
     {
         path: "/",
-        element: <Protected><Home /></Protected>
+        element: (
+            <Protected>
+                <Home />
+            </Protected>
+        )
     },
     {
-        path:"/interview/:interviewId",
-        element: <Protected><Interview /></Protected>
+        path: "/interview/:interviewId",
+        element: (
+            <Protected>
+                <Interview />
+            </Protected>
+        )
     }
-])
+]);
